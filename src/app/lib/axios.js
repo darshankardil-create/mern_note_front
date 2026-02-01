d138e4ff62513caf8a32e54efc0804f9c5a130ae
darshankardil-create <darshan.kardil@gmail.com>
2026-02-01 22:00:25 +0530

    Add files via upload

---
+++ src/app/lib/axios.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://mern-note-2-2.onrender.com/api",
+});
+
+export default api;
+
+//"https://mern-note-1-0.onrender.com/api"
+
+
+// 123         https://mern-note-2-7py2.onrender.com/api
+
+
+// http://localhost:3000/api
